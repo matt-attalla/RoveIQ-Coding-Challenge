84eca324e22b9ad4c66803038eefa9aef5439860
Matt Attalla <matt.attalla23@gmail.com>
2022-09-07 15:13:36 -0700

    Added the Locations, SelectedLocation, and Advertisements component to the app. Also getting data from roveiq endpoint on component mount.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect } from 'react';
+import Locations from './components/Locations/Locations';
+import SelectedLocation from './components/SelectedLocation/SelectedLocation';
+import Advertisements from './components/Advertisements/Advertisements';
 import './App.css';
+import { useDispatch } from 'react-redux';
+import { getDirectoryData } from './features/DirectorySlice';
+import { AppDispatch } from './store';
 
 function App() {
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect( () => {
+    dispatch(getDirectoryData())
+  }, [dispatch])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="row app-container">
+      <div className="col-lg-4 location-container">
+        <Locations/>
+      </div>
+      <div className="col-lg-4">
+        <SelectedLocation/>
+      </div>
+      <div className="col-lg-4">
+        <Advertisements/>
+      </div>
     </div>
   );
 }
